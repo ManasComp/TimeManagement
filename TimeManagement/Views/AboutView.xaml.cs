1673949c8717059ad4959fbd9d60a700ff74d37d
Manas <ManasComp1@gmail.com>
2020-09-20 21:14:37 +0200

    changes

---
+++ TimeManagement/Views/AboutView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodOrderApp.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TimeManagement.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutView : ContentPage
+    {
+        public AboutView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            new PageService().PopModalAsync();
+        }
+    }
+}
---
+++ TimeManagement/Views/ShellView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodOrderApp.Services;
+using TimeManagement.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TimeManagement.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShellView : Xamarin.Forms.Shell
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+
+        private async void RefreshData_OnClicked(object sender, EventArgs e)
+        {
+           await  new SettingsViewModel().LoadNewData();
+        }
+        private void Logout_OnClicked(object sender, EventArgs e)
+        {
+            new PageService().PushModalAsync(new LogoutView());
+        }
+        private void About_OnClicked(object sender, EventArgs e)
+        {
+            new PageService().PushModalAsync(new NavigationPage(new AboutView()));
+        }
+        
+    }
+}
