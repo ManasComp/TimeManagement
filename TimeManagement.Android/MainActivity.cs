079084ef6ef267c44a03e5292ba5ec12ee3b232a
Manas <ManasComp1@gmail.com>
2020-10-26 16:46:12 +0100

    logo

--- TimeManagement.Android/MainActivity.cs
+++ TimeManagement.Android/MainActivity.cs
@@ -9,7 +9,8 @@ using Android.OS;
 
 namespace TimeManagement.Droid
 {
-    [Activity(Label = "TimeManagement", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "TimeManagement")]
+    // , Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
